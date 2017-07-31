3489953131c61ec5c0ae354d7149349b135cdfb0
Mathias Dahlström <mathias@monsieurdahlstrom.com>
2017-07-31 19:18:37 +0800

    removed models
    removed redundant  models.

--- src/models/WebBluetoothStoreUtils.js
+++
@@ -1,9 +0,0 @@
-const Cannonical128UUIDBase = '0000XXXX-0000-1000-8000-00805f9b34fb'
-
-const WebBluetoothStoreUtils = {
-  cannonicalUUIDFor16bitUUID: function (UUID16) {
-    return Cannonical128UUIDBase.replace('XXXX', UUID16.toString(16))
-  }
-}
-
-export default WebBluetoothStoreUtils
