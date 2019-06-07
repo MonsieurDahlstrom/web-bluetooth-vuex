9e3b2bbd9a52341d392420a48cafd44589b5b645
Mathias Dahlström <mathias.dahlstrom@gmail.com>
2019-06-07 12:39:40 +0200

    rollup for bundling

---
+++ rollup.config.js
@@ -0,0 +1,23 @@
+// rollup.config.js
+import resolve from 'rollup-plugin-node-resolve';
+import babel from 'rollup-plugin-babel';
+
+export default {
+  input: 'src/index.js',
+  output: [
+      {
+        file: 'dist/esm.bundle.js',
+        format: 'esm'
+      },
+      {
+        file: 'dist/cjs.bundle.js',
+        format: 'cjs'
+      }
+  ],
+  plugins: [
+    resolve(),
+    babel({
+      exclude: 'node_modules/**' // only transpile our source code
+    })
+  ]
+};
